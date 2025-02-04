8f9ad9f674209c9872178b78deb52823e6275c52
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-02-04 12:55:42 +0100

    add IndividualMovie component

---
+++ src/movies/IndividualMovie.tsx
@@ -0,0 +1,16 @@
+import { movieDTO } from "./movies.model";
+
+export default function IndividualMovie(props: movieDTO) {
+  const buildLink = () => `/movie${props.id}`;
+
+  return (
+    <div>
+      <a href={buildLink()}>
+        <img alt="Poster" src={props.poster} />
+      </a>
+      <p>
+        <a href={buildLink()}>{props.title}</a>
+      </p>
+    </div>
+  );
+}
