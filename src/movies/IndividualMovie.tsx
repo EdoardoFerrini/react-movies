1f7d2a50530f1ef1f30cd29d3181fb6128632701
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-02-04 12:58:28 +0100

    add IndividualMovie css

--- src/movies/IndividualMovie.tsx
+++ src/movies/IndividualMovie.tsx
@@ -1,10 +1,11 @@
 import { movieDTO } from "./movies.model";
+import css from "./IndividualMovie.module.css";
 
 export default function IndividualMovie(props: movieDTO) {
   const buildLink = () => `/movie${props.id}`;
 
   return (
-    <div>
+    <div className={css.div}>
       <a href={buildLink()}>
         <img alt="Poster" src={props.poster} />
       </a>
