c74340f49339a40ec38e74412d97e823e6c534e8
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-02-04 13:03:45 +0100

    add hard-coded data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import IndividualMovie from "./movies/IndividualMovie";
+import { movieDTO } from "./movies/movies.model";
 
 function App() {
+  const testMovie: movieDTO = {
+    id: 1,
+    title: "Spider-Man: Far From Home",
+    poster:
+      "https://posterspy.com/wp-content/uploads/2019/07/Farfromhome3D.jpg",
+  };
+
   return (
     <>
-      <h2>Hello world</h2>
+      <IndividualMovie {...testMovie} />
     </>
   );
 }
