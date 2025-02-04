cf808d0fbba204a155b752a622fcf40b27d0fa7d
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-02-04 14:05:42 +0100

    add hard-coded data landingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
-import IndividualMovie from "./movies/IndividualMovie";
-import { movieDTO } from "./movies/movies.model";
+import { useEffect, useState } from "react";
+import { landingPageDTO } from "./movies/movies.model";
 import MoviesList from "./movies/MoviesList";
 
 function App() {
-  const inTheaters: movieDTO[] = [
-    {
-      id: 1,
-      title: "Spider-Man: Far From Home",
-      poster:
-        "https://posterspy.com/wp-content/uploads/2019/07/Farfromhome3D.jpg",
-    },
-    {
-      id: 2,
-      title: "Interstellar",
-      poster:
-        "https://posterspy.com/wp-content/uploads/2024/12/INTERSTELLAR-NO-MUR.jpg",
-    },
-  ];
+  const [movies, setMovies] = useState<landingPageDTO>({});
 
-  const upcomingReleases: movieDTO[] = [
-    {
-      id: 3,
-      title: "Inception",
-      poster: "https://posterspy.com/wp-content/uploads/2024/05/inception.jpg",
-    },
-  ];
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMovies({
+        inTheaters: [
+          {
+            id: 1,
+            title: "Spider-Man: Far From Home",
+            poster:
+              "https://posterspy.com/wp-content/uploads/2019/07/Farfromhome3D.jpg",
+          },
+          {
+            id: 2,
+            title: "Interstellar",
+            poster:
+              "https://posterspy.com/wp-content/uploads/2024/12/INTERSTELLAR-NO-MUR.jpg",
+          },
+        ],
+        upcomingReleases: [
+          {
+            id: 3,
+            title: "Inception",
+            poster:
+              "https://posterspy.com/wp-content/uploads/2024/05/inception.jpg",
+          },
+        ],
+      });
+    }, 1000);
+  });
 
   return (
     <>
       <h2>In Theathers</h2>
-      <MoviesList movies={inTheaters} />
+      <MoviesList movies={movies.inTheaters} />
       <h2>Upcoming Releases</h2>
-      <MoviesList movies={upcomingReleases} />
+      <MoviesList movies={movies.upcomingReleases} />
     </>
   );
 }
