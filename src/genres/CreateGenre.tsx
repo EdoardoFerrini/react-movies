df06ed71369c47981e2768bd2db7bff96371adff
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-02-05 21:59:38 +0100

    useNavigate for navigation

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../utils/Button";
+
 export default function CreateGenre() {
+  const navigate = useNavigate();
   return (
     <>
       <h2>Create Genre</h2>
+      <Button onClick={() => navigate("/genres")}>Save Changes</Button>
     </>
   );
 }
